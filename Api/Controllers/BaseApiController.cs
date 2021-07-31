a2163452e0ccde83bc2e544e1ae57a05605ccd4b
Jordan Raine <raine.jordan@gmail.com>
2021-07-31 23:10:43 +1000

    MediatR

--- Api/Controllers/BaseApiController.cs
+++ Api/Controllers/BaseApiController.cs
@@ -1,4 +1,6 @@
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Api.Controllers
 {
@@ -6,6 +8,9 @@ namespace Api.Controllers
     [Route("api/[controller]")]
     public class BaseApiController : ControllerBase
     {
-        
+        private IMediator mediator;
+
+        protected IMediator Mediator => mediator ??= HttpContext.RequestServices
+            .GetService<IMediator>();
     }
 }
--- Api/Startup.cs
+++ Api/Startup.cs
@@ -2,6 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Api.Extensions;
+using Application.Activities;
+using Application.Core;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -29,23 +33,7 @@ namespace Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Api", Version = "v1" });
-            });
-
-            services.AddDbContext<DataContext>(opt =>
-            {
-                opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
-            });
-
-            services.AddCors(opt =>
-            {
-                opt.AddPolicy("CorsPolicy", policy =>
-                {
-                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-                });
-            });
+            services.AddApplicationServices(config);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
