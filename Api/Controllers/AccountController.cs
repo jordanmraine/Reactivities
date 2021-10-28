4338162b9ac40591c21ea714c6a1656ffda15c7a
Jordan Raine <raine.jordan@gmail.com>
2021-10-28 21:07:16 +1100

    Front-end login & registration

--- Api/Controllers/AccountController.cs
+++ Api/Controllers/AccountController.cs
@@ -49,12 +49,14 @@ namespace Api.Controllers
         {
             if (await userManager.Users.AnyAsync(u => u.Email == registerDto.Email))
             {
-                return BadRequest("Email taken");
+                ModelState.AddModelError("email", "Email taken");
+                return ValidationProblem(ModelState);
             }
 
             if (await userManager.Users.AnyAsync(u => u.UserName == registerDto.Username))
             {
-                return BadRequest("Username taken");
+                ModelState.AddModelError("username", "Username taken");
+                return ValidationProblem(ModelState);
             }
 
             var user = new AppUser
